c8e634101d07b42c2a7ac2219a2d11bbf850865e
Thomas Mullaly <thomas.mullaly@gmail.com>
2017-07-15 12:08:21 -0400

    Improved go app to 404 for resource requests instead of redirecting to 404 page

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 )
 
 func init() {
@@ -9,5 +10,13 @@ func init() {
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/404.html", http.StatusFound)
+	ext := filepath.Ext(r.URL.Path)
+
+	// Send them to the 404 page if it looks like a page was being requested.
+	// Otherwise, 404.
+	if ext == "html" || ext == "htm" || ext == "" {
+		http.Redirect(w, r, "/404.html", http.StatusFound)
+	} else {
+		http.NotFound(w, r)
+	}
 }
