c96eab4c524be68d0d0f63c20854bef6517dd409
Thomas Mullaly <thomas.mullaly@gmail.com>
2019-04-24 21:00:30 -0400

    Upgrading to latest golang runtime

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thomas-mullaly/thomasmullaly.codes
+
+go 1.12
+
+require google.golang.org/appengine v1.5.0
--- web/main.go
+++ web/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
-func init() {
+func main() {
 	http.HandleFunc("/", redirect)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
